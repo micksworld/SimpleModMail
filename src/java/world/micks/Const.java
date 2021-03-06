b11c3152505558ec4263be3aa96116537aee0eee
Mick <47728875+iDevMC@users.noreply.github.com>
2021-03-06 09:40:34 -0500

    Create Const.java

---
+++ src/java/world/micks/Const.java
@@ -0,0 +1,10 @@
+package world.micks;
+
+import java.awt.Color;
+
+public class Const {
+
+	public static final long CHANNELID = 817570386668814346l;
+	public static final Color color = new Color(37, 153, 54);
+	
+}
